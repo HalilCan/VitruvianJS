83764fc1208cef67b7bd893465fa083589e0f756
HalilCan <halilcanmemoglu@gmail.com>
2020-02-09 15:44:33 -0500

    switch to aero

--- src/recorder.js
+++ src/recorder.js
@@ -76,6 +76,35 @@ function setStopButton() {
     }, false);
 }
 
+function localFileVideoPlayer() {
+	'use strict'
+  let URL = window.URL || window.webkitURL
+  let displayMessage = function (message, isError) {
+    let element = document.querySelector('#message')
+    element.innerHTML = message
+    element.className = isError ? 'error' : 'info'
+  }
+  var playSelectedFile = function (event) {
+    var file = this.files[0]
+    var type = file.type
+    var videoNode = document.querySelector('video')
+    var canPlay = videoNode.canPlayType(type)
+    if (canPlay === '') canPlay = 'no'
+    var message = 'Can play type "' + type + '": ' + canPlay
+    var isError = canPlay === 'no'
+    displayMessage(message, isError)
+
+    if (isError) {
+      return
+    }
+
+    var fileURL = URL.createObjectURL(file)
+    videoNode.src = fileURL
+  }
+  var inputNode = document.querySelector('input')
+  inputNode.addEventListener('change', playSelectedFile, false)
+}
+
 function setUploadButton() {
     
 }
@@ -94,5 +123,4 @@ window.addEventListener('load',
         setStartButtonAsRecorder();
         setStopButton();
         setUploadButton();
-
     }, false);
---
+++ todo.md
@@ -0,0 +1,8 @@
+##TODO
+
+
+* uploaded analysis
+* uploaded poseGet callback
+* skeleNormalizer (relative positioning conversion)
+* areaDrawer
+* antiJitterer
